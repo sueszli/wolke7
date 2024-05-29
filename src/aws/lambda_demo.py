e88b22b6fc43bbdc4b82a58fe84f6a2366c29267
sueszli <jabaryyahya@gmail.com>
2024-05-29 02:31:40 +0200

    up

--- src/aws/lambda_demo.py
+++ src/aws/lambda_demo.py
@@ -2,6 +2,30 @@ import boto3
 from colorama import Fore, Style
 
 
+"""
+# create demo lambda function
+echo 'def lambda_handler(event, context): print(event)' > lambda_function.py
+zip lambda_function.zip lambda_function.py
+
+# note your own account id
+aws sts get-caller-identity
+
+# create a lambda function
+# change `<YOUR ACCOUNT ID HERE>` with your own account id
+# notice how we're using the existing `LabRole` role
+aws lambda create-function --function-name my-function --runtime python3.8 --role arn:aws:iam::<YOUR ACCOUNT ID HERE>:role/LabRole --handler lambda_function.lambda_handler --zip-file fileb://lambda_function.zip
+
+# invoke the lambda function and print the output
+aws lambda invoke --function-name my-function response.json && rm -rf response.json
+
+# show existing lambda functions
+aws lambda list-functions
+
+# delete the lambda function
+aws lambda delete-function --function-name my-function
+"""
+
+
 def get_account_id():
     return boto3.client("sts").get_caller_identity()["Account"]
 
--- src/aws/s3_demo.py
+++ src/aws/s3_demo.py
@@ -4,6 +4,23 @@ from colorama import Fore, Style
 from tqdm import tqdm
 
 
+"""
+# create a bucket
+aws s3api create-bucket --bucket wolke-sieben --region us-east-1
+
+# show buckets and objects
+aws s3api list-buckets
+aws s3api list-objects --bucket wolke-sieben
+
+# upload a directory
+aws s3 cp ./data/input_folder s3://wolke-sieben/input_folder --recursive
+
+# empty bucket and delete it
+aws s3 rm s3://wolke-sieben --recursive
+aws s3api delete-bucket --bucket wolke-sieben
+"""
+
+
 def create_bucket(bucket_name):
     print(f"{Fore.GREEN}creating bucket {bucket_name}{Style.RESET_ALL}")
 
