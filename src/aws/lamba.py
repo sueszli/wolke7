d2f0cf475af1ba7541a1c46a4214eb4355c6fa99
sueszli <jabaryyahya@gmail.com>
2024-05-29 11:25:34 +0200

    up

--- src/aws/lamba.py
+++ src/aws/lamba.py
@@ -27,14 +27,56 @@ aws lambda list-functions
 aws lambda delete-function --function-name my-function
 """
 
+client = boto3.client("lambda", region_name="us-west-1")
 
-def get_account_id():
-    return boto3.client("sts").get_caller_identity()["Account"]
+
+def create_lambda_function(function_name, zip_file):
+    print(f"{Fore.GREEN}creating lambda function {function_name}{Style.RESET_ALL}")
+
+    accountid = boto3.client("sts").get_caller_identity()["Account"]
+    role = "LabRole"
+
+    with open(zip_file, "rb") as f:
+        response = client.create_function(
+            FunctionName=function_name,
+            Runtime="python3.8",
+            Role=f"arn:aws:iam::{accountid}:role/{role}",
+            Handler="lambda_function.lambda_handler",
+            Code={"ZipFile": f.read()},
+        )
+
+    print(f"created lambda function {function_name}")
+    return response["FunctionArn"]
+
+
+def invoke_lambda_function(function_name):
+    print(f"{Fore.GREEN}invoking lambda function {function_name}{Style.RESET_ALL}")
+
+    response = client.invoke(FunctionName=function_name)
+
+    print(f"invoked lambda function {function_name}")
+    return response
+
+
+def list_lambda_functions():
+    print(f"{Fore.GREEN}listing lambda functions{Style.RESET_ALL}")
+
+    response = client.list_functions()
+
+    print(f"listed lambda functions")
+    return response
+
+
+def delete_lambda_function(function_name):
+    print(f"{Fore.GREEN}deleting lambda function {function_name}{Style.RESET_ALL}")
+
+    response = client.delete_function(FunctionName=function_name)
+
+    print(f"deleted lambda function {function_name}")
+    return response
 
 
 if __name__ == "__main__":
-    print(f"account id: {get_account_id()}")
-
     # create temporary zip to upload function
     lambdapath = Path.cwd() / "src" / "aws" / "lambda_function.py"
     lambdazip = lambdapath.with_suffix(".zip")
@@ -43,6 +85,15 @@ if __name__ == "__main__":
     with zipfile.ZipFile(lambdazip, "w") as z:
         z.write(lambdapath, lambdapath.name)
     print(f"{Fore.GREEN}created zip {lambdazip}{Style.RESET_ALL}")
+
+    function_name = "my-function"
+    create_lambda_function(function_name, lambdazip)
+
+    invoke_lambda_function(function_name)
+
+    list_lambda_functions()
+
+    delete_lambda_function(function_name)
 
     # delete zip file
     if lambdazip.exists():
